2f559fae96ba90ac55d89f0da6e59b9a950f52e1
BABUTSIDZ3 <babutsidzegia7@gmail.com>
2024-05-29 07:44:22 -0700

    frontend

--- src/componetns/header.tsx
+++ src/componetns/header.tsx
@@ -1,7 +1,9 @@
 import logo  from '../assets/logo.png'
 import './header.css'
+import {  Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBarsStaggered } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { useRef } from 'react';
 
 const Header = () => {
@@ -14,9 +16,10 @@ const Header = () => {
     return (
       <>
         <header className="header">
-          <img className='logo' src={logo} alt="logo" />
-          <nav ref={navRef}  className="header-nav"> 
-
+          <Link to={"/"}>
+            <img className="logo" src={logo} alt="logo" />
+          </Link>
+          <nav ref={navRef} className="header-nav">
             <ul className="header-nav-ul">
               <li>
                 <h1>ფილმები</h1>
@@ -28,12 +31,20 @@ const Header = () => {
                 <h1>ანიმაციები</h1>
               </li>
               <li>
-                <input type="text"/>
+                <input type="text" />
+              </li>
+              <li>
+                <Link to={"/registration"}>
+                  <FontAwesomeIcon icon={faUser} />
+                </Link>
               </li>
             </ul>
-           
           </nav>
-          <FontAwesomeIcon onClick={showNavbar}  className='nav-bar' icon={faBarsStaggered} />
+          <FontAwesomeIcon
+            onClick={showNavbar}
+            className="nav-bar"
+            icon={faBarsStaggered}
+          />
         </header>
       </>
     );
